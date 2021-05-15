7215bba19a40eb3f637396d2a40ad25ff483a374
Prasad Talamala <vtalamala@apple.com>
2021-05-15 23:00:59 +0530

    removed unwanted files

--- portfolio_personal/settings.py
+++ portfolio_personal/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+else:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
