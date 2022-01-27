72ff8b1b56d4259de9a53eef15f5f0f5b7277e27
prasad-talamala <prasad49.talamala@gmail.com>
2022-01-27 15:10:28 +0530

    redesigned website version 2.0

--- portfolio_personal/urls.py
+++ portfolio_personal/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 from project import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+
+    # Media Files Handling
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
